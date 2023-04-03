54a89a6575b3accd831d969c13712a0eabd2399b
Jeffrey Neal <jeffneal11@gmail.com>
2023-04-03 15:14:00 -0500

    comp email

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -4,6 +4,7 @@ import com.codeup.codeupspringblog.models.Post;
 import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import com.codeup.codeupspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +20,25 @@ public class PostController {
     PostRepository postDAO;
     UserRepository userDAO;
 
-    public PostController(PostRepository postDAO, UserRepository userDAO){
+    private EmailService emailService;
+
+//    public PostController() {
+//        this.emailService = emailService;
+//    }
+
+    public PostController(PostRepository postDAO, UserRepository userDAO,EmailService emailService){
 
         this.postDAO = postDAO;
         this.userDAO = userDAO;
+        this.emailService = emailService;
     }
 
 
+@GetMapping(path = "/email")
+public String sendEmail(){
+    emailService.prepareAndSend("subjecttttt", "get shrimpeed");
+    return "redirect:/posts";
+}
 
 
     @GetMapping(path = "/posts")
@@ -87,42 +100,18 @@ public class PostController {
 
 
     @RequestMapping(path = "/posts/{id}/edit", method = RequestMethod.GET)
-//    @ResponseBody
     public String showEditForm(@PathVariable int id,Model model) {
         Post post = postDAO.findById(id).get();
-//        model.addAttribute("id",id);
-//        model.addAttribute("post", post);
-//        model.addAttribute("creator", post.getOwner());
         model.addAttribute("post", post);
-
         return "/posts/edit";
     }
 
     @RequestMapping(path = "/posts/{id}/edit", method = RequestMethod.POST)
-//    @ResponseBody
     public String edit(@ModelAttribute Post post) {
-
-//        User creator = userDAO.findById(1L).get();
-//        System.out.println(post.getBody());
-//        System.out.println(post.getTitle());
-//        post.setOwner(creator);
-//            Post post = new Post(title, body, creator);
         postDAO.save(post);
-//        if(creator.getPosts().size() > 0){
-//            List<Post> posts = creator.getPosts();
-//            posts.add(post);
-//            creator.setPosts(posts);
-//            userDAO.save(creator);
-//
-//        }else{
-//            List<Post> posts = new ArrayList<>();
-//            posts.add(post);
-//            creator.setPosts(posts);
-//            userDAO.save(creator);
-//
-//        }
-//        System.out.println(creator.getPosts());
         return "redirect:/posts";
     }
+
+
 
 }
